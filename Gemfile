fe2b09d5a4a9d4af02e79b9c586410bc190132a5
cychung18@gmail.com <cychung18@gmail.com>
2019-10-28 19:43:03 -0500

    Setup Devise Gem

--- Gemfile
+++ Gemfile
@@ -5,6 +5,15 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+ruby '2.4.6'
+###############
+gem 'devise'
+gem 'carrierwave'
+gem 'bootstrap-sass'
+gem 'jquery-rails'
+gem 'font-awesome-rails'
+gem 'better_errors', group: :development
+###############
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.7'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191024002816) do
+ActiveRecord::Schema.define(version: 20191029002204) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,18 @@ ActiveRecord::Schema.define(version: 20191024002816) do
     t.text "blog_entry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
